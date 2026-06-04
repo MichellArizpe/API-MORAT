61f780ce6895860ab36b2ebdfe6c2ad45a9c92dc
MichellArizpe <alynarizpe@gmail.com>
2026-06-04 16:49:43 -0600

    Archivo cat controller actualizado

---
+++ src/cats/cats.contoller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body
+} from '@nestjs/common';
+
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+
+  constructor(
+    private readonly catsService: CatsService
+  ) {}
+
+  @Get()
+  findAll() {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.catsService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCatDto: UpdateCatDto
+  ) {
+    return this.catsService.update(
+      Number(id),
+      updateCatDto
+    );
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.catsService.remove(Number(id));
+  }
+}
